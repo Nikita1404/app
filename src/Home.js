a980b5e57159b645c1c1b58fc71b47e14cf8bb39
artem_zakholodilo <test@test.com>
2020-01-11 16:27:14 +0200

    v1

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form';
+
+const Home = () => {
+
+    return (
+        <>
+            <h1>Hello footer</h1>
+            <Form />
+        </>
+    );
+}
+
+export default Home;
---
+++ src/data.js
@@ -0,0 +1,13 @@
+const send = () => {
+    alert('SEND');
+}
+
+export const format = () => {
+    alert('FORMAT');
+}
+
+export const getData = () => {
+    alert('GET_DATA');
+}
+
+export default send;
